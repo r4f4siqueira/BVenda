47b32bd8ad48447f64575e01f4ca2ef2f3af026c
Rafael Siqueira <51487888+r4f4siqueira@users.noreply.github.com>
2021-10-25 21:55:18 -0400

    Telas Oficiais

---
+++ app/src/main/java/uakas/com/bvenda/contas.java
@@ -0,0 +1,14 @@
+package uakas.com.bvenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contas);
+    }
+}
