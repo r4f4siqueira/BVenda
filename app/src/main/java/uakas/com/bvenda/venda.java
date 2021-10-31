5fd84f8770ee71899b53e9db8ef8f5f3df02dfd5
Rafael Siqueira <51487888+r4f4siqueira@users.noreply.github.com>
2021-10-31 14:59:41 -0400

    Adicionado btMenu nos layouts

--- app/src/main/java/uakas/com/bvenda/venda.java
+++ app/src/main/java/uakas/com/bvenda/venda.java
@@ -2,7 +2,9 @@ package uakas.com.bvenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class venda extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class venda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venda);
+    }
+
+    public void menul (View view){
+        Intent it = new Intent(this, menuLateral.class);
+        startActivityForResult(it,101);
     }
 }
